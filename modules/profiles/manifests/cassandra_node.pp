274efbd2503828a70bf7c331c17ad54d8da28b35
Bryan Wyatt <brwyatt@gmail.com>
2015-08-20 01:24:20 -0700

    Fix Cassandra config options

--- modules/profiles/manifests/cassandra_node.pp
+++ modules/profiles/manifests/cassandra_node.pp
@@ -1,8 +1,19 @@
 class profiles::cassandra_node {
   class { 'cassandra':
-    cluster_name => 'vagrantCluster',
-    package_name => 'dsc21',
-    seeds        => [ '10.1.1.101', '10.1.2.101', '10.1.3.101' ],
-    version      => '2.1.8-1',
+    cluster_name      => 'vagrantCluster',
+    package_name      => 'dsc21',
+    seeds             => [ '10.1.1.101', '10.1.2.101', '10.1.3.101' ],
+    version           => '2.1.8-1',
+    listen_address    => $::ipaddress_eth1,
+    broadcast_address => $::ipaddress_eth1,
+    rpc_address       => $::ipaddress_eth1,
+    rpc_max_threads   => 128,
   }
+  file { '/usr/share/cassandra/lib/jamm-0.2.8.jar':
+    ensure => link,
+    target => '/usr/share/cassandra/lib/jamm-0.3.0.jar',
+    notify => Service['cassandra'],
+  }
+
+  Package['dsc21'] -> File['/usr/share/cassandra/lib/jamm-0.2.8.jar']
 }
